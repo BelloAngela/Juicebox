554a0ef184ff72a0d2f6ebdb468626144e83d6bb
BelloAngela <bellotile@mail.com>
2024-06-05 15:25:26 -0700

    Add files via upload

---
+++ api/posts.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const postsRouter = express.Router();
+
+const { requireUser } = require('./utils');
+
+const { 
+  createPost,
+  getAllPosts,
+  updatePost,
+  getPostById,
+  createTags,
+  addTagsToPost
+} = require('../db');
+
+postsRouter.get('/', async (req, res, next) => {
+  try {
+    const allPosts = await getAllPosts();
+
+    const posts = allPosts.filter(post => {
+      // the post is active, doesn't matter who it belongs to
+      if (post.active) {
+        return true;
+      }
+    
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+    
+      // none of the above are true
+      return false;
+    });
+  
+    res.send({
+      posts
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content = "",tags [] } = req.body;
+
+  const postData = {};
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+
+    const post = await createPost(postData);
+
+    if (post) {
+      if (tags && tags.length > 0) {
+        const tagList = await createTags(tags);
+        await addTagsToPost(post.id, tagList);
+      }
+      res.send(post);
+    } else {
+      next({
+        name: 'PostCreationError',
+        message: 'There was an error creating your post. Please try again.'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+
+    const post = await getPostById(postId);
+    if (!post) {
+        return res.status(404).send({ error: 'Post not found' });
+    }
+    if (post.author.id !== req.user.id) {
+        return res.status(403).send({ error: 'You do not have permission to delete this post' });
+    }
+    const deletedPost = await deletePost(postId);
+    res.status(204).send(deletedPost);  // 204 No Content on successful delete
+} catch ({ name, message }) {
+    next({ name, message });
+}
+});
+
+module.exports = postsRouter;
